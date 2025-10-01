ca9c692333f4c5b6e9961c0b2629f04091f542fb
sadik ansari <mohammedsadikansari@75gmail.com>
2025-10-01 11:09:24 +0530

    Add two sum solutions in js (#1)

---
+++ Arrays/twoSum.js
@@ -0,0 +1,12 @@
+// Problem: Two Sum (#1) - https://leetcode.com/problems/two-sum/description/
+
+var twoSum = function(nums, target) {
+    
+  for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+};
