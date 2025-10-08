b83f90a995369fc9f91c47402f0640ff5478e77c
sadik ansari <mohammedsadikansari75@gmail.com>
2025-10-08 10:45:37 +0530

    Second approach for solving Left Rotation by K in JS solution

--- Arrays/leftRotationByK.js
+++ Arrays/leftRotationByK.js
@@ -1,9 +1,9 @@
-// Problem: Rotaion by K element
+// Problem: Left Rotaion by K element
 
 let arr = [1, 2, 3, 4, 5];
 let move = null;
 let move2 = null;
-let n = 2
+let n = 3
 for (let i = 1; i <= n; i++) {
     for (let i = arr.length - 1; i >= 0; i--) {
         if (i > 0 && move === null) {
@@ -21,3 +21,22 @@ for (let i = 1; i <= n; i++) {
     }
 }
 console.log(arr)
+
+// Output using deffrent method
+
+let arr1 = [1, 2, 3, 4, 5];
+let newArr = new Array(arr1.length)
+let k = 3 % arr1.length
+for (let i = arr1.length - 1; i >= 0; i--) {
+    let value = arr1[i]
+    let j = i
+    j = j - k;
+    if(j >= 0){
+        newArr[j] = value
+    }
+    else if(j < 0){
+        j = newArr.length - (-1 * j)
+        newArr[j] = value
+    }
+}
+console.log(newArr)
--- Arrays/rightRotationByK.js
+++ Arrays/rightRotationByK.js
@@ -1,7 +1,7 @@
 // Problem: Right Rotation by K
 
 let arr = [1,2,3,4,5];
-let k = 3;
+let k = 2%arr.length;
 for(let j = 1; j <= k; j++){
     let copy = arr[arr.length-1]
     for(let i = arr.length -1; i > 0; i--){
