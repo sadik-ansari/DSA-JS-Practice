f6537275fd6630ac3590aee7c723aa8160e531bb
sadik ansari <mohammedsadikansari75@gmail.com>
2025-10-10 21:11:13 +0530

    Rotate Array in JS solution(#189)

---
+++ Arrays/rotateArray.js
@@ -0,0 +1,18 @@
+// Problem: Rotate Array (#189) - https://leetcode.com/problems/rotate-array/description/
+
+var rotate = function (nums, k) {
+    k = k % nums.length
+    function reverse (nums, start, end){
+        while(start < end){
+             let temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++
+            end--
+        }
+        nums
+    }
+    reverse(nums, 0, nums.length-1)
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1)
+};
