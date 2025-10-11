ddf17e9fd668e5cebacf12e44bd3156d55c37391
sadik ansari <mohammedsadikansari75@gmail.com>
2025-10-11 20:50:19 +0530

    Traipping Rain Water in JS solution with second approach (#42)

--- Arrays/majorityElement.js
+++ Arrays/majorityElement.js
@@ -1,25 +1,5 @@
 // Problem: Majority Element (#169) - https://leetcode.com/problems/majority-element/description/
 
-var majorityElement = function(nums) {
-    let n = nums.length/2;
-    let counter = 0
-    for(let i = 0; i < nums.length; i++){
-        for(let j = 0; j < nums.length; j++){
-            if(nums[i] === nums[j]){
-                ++counter
-            }
-        }
-        console.log(counter, nums[i])
-        if(n < counter){
-            n = nums[i]
-            return n
-            break;
-        }
-    }
-    
-};
-console.log(majorityElement([8,8,7,7,7]))
-
 // Boyer–Moore (Moore’s Voting) Algorithm
 
 var majorityElement = function(nums) {
--- Arrays/traippingRainWater.js
+++ Arrays/traippingRainWater.js
@@ -1,4 +1,4 @@
-// Problem: Traipping Rain Water (#217) - https://leetcode.com/problems/trapping-rain-water/description/
+// Problem: Traipping Rain Water (#42) - https://leetcode.com/problems/trapping-rain-water/description/
 
 var trap = function (height) {
     let left = 0;
@@ -30,4 +30,33 @@ var trap = function (height) {
     return result;
 }
 
+console.log(trap([3, 0, 2, 0, 4]));
+
+//Second Approach
+
+var trap = function (height) {
+    let left = new Array(height.length);
+    let right = new Array(height.length);
+    let leftMax = height[0];
+    let rightMax = height[height.length - 1];
+    left[0] = leftMax;
+    right[right.length - 1] = rightMax
+    let result = 0;
+
+    for (let i = 1; i < height.length; i++) {
+        leftMax = Math.max(leftMax, height[i])
+        left[i] = leftMax
+    }
+    for (let i = height.length - 2; i >= 0; i--) {
+        rightMax = Math.max(rightMax, height[i])
+        right[i] = rightMax
+    }
+
+    for (let i = 0; i < height.length; i++) {
+        result += Math.min(left[i], right[i]) - height[i]
+    }
+
+    return result;
+
+    }
 console.log(trap([3, 0, 2, 0, 4]));
