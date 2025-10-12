3fc83a5d339bb18f2e28331c2145a2c57ad2cc1c
sadik ansari <mohammedsadikansari75@gmail.com>
2025-10-12 19:10:02 +0530

    Second Approach of Product of Array Except Self in jS solution (#238)

--- Arrays/productOfArrayExceptSelf.js
+++ Arrays/productOfArrayExceptSelf.js
@@ -15,3 +15,27 @@ var productExceptSelf = function(nums) {
     return answer
 };
 
+
+//Second Approach
+var productExceptSelf = function(nums) {
+    let i = j = 0;
+    let answer = [];
+    let cal = 1
+    while(i < nums.length){
+       answer[i] = (multi(i, j));
+       i++
+    }
+    function multi (i, j){
+        cal = 1
+        while(j < nums.length){
+             if(j != i){
+                cal = cal * nums[j]
+            }
+            j++
+        }
+        return cal
+    }
+   
+    return answer
+};
+console.log(productExceptSelf([1,2,3,4]))
