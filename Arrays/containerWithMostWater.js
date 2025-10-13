8956ec613a4e8c0e839614133a01d3e8240e3170
sadik ansari <mohammedsadikansari75@gmail.com>
2025-10-13 17:49:08 +0530

    Container with Most Water in JS solution (#11)

---
+++ Arrays/containerWithMostWater.js
@@ -0,0 +1,18 @@
+// Problem: Container with Most Water (#169) - https://leetcode.com/problems/container-with-most-water/description/
+
+var maxArea = function (height) {
+    let answer = 0;
+    let i = 0;
+    let j = height.length - 1;
+    while (i != j) {
+        if (height[i] < height[j]) {
+            answer = Math.max(answer, ((j - i) * height[i]))
+            i++
+        }
+        else {
+            answer = Math.max(answer, ((j - i) * height[j]))
+            j--
+        }
+    }
+    return answer
+};
