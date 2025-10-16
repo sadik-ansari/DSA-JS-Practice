9c7b263130e58aa16599f802aac4b1755ebb746a
sadik ansari <mohammedsadikansari75@gmail.com>
2025-10-16 22:00:13 +0530

    Valid Palindrome in JS solution (#125)

---
+++ Strings/validPalindrome.js
@@ -0,0 +1,17 @@
+// Problem: Valid Palindrome (#242) - https://leetcode.com/problems/valid-palindrome/description/
+
+var isPalindrome = function(s) {
+    s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let left = 0;
+    let right = s.length - 1
+    while(left < right){
+        if(s[left] != s[right]){
+            return false;
+        }
+        else{
+            right--;
+            left++;
+        }
+    }
+    return true;
+};
