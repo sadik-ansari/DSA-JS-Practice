73347b49c2e17f7950aa7c1a9a9f49b0143b4926
sadik ansari <mohammedsadikansari75@gmail.com>
2025-10-17 20:41:58 +0530

    Reverse String in JS solution (#344)

---
+++ Strings/reverseString.js
@@ -0,0 +1,13 @@
+// Problem: Reverse String (#344) - https://leetcode.com/problems/reverse-string/description/
+
+var reverseString = function (s) {
+    let i = 0;
+    let j = s.length - 1
+    while (i < j) {
+        let temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+        i++;
+        j--;
+    }
+};
