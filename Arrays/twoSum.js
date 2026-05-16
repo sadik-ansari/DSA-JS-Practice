5e6deaadd5b67a6d51da566fc0bd2fa9ccdf85d9
sadik ansari <mohammedsadikansari75@gmail.com>
2026-05-16 07:13:33 +0530

    Solved the Two sum problem with Hash Map

--- Arrays/twoSum.js
+++ Arrays/twoSum.js
@@ -1,12 +1,18 @@
 // Problem: Two Sum (#1) - https://leetcode.com/problems/two-sum/description/
 
-var twoSum = function(nums, target) {
-    
-  for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
+var twoSum = function (nums, target) {
+    let hasMap = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i]
+
+        if(hasMap.has(complement)){
+            return [hasMap.get(complement), i]
         }
+
+        hasMap.set(nums[i], i)
     }
-};
+
+}; 
+
+console.log(twoSum([2, 7, 11, 15], 22))
