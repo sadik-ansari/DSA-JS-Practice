6f159d8000089709291a22926fd32e7cdb7d0ad2
sadik ansari <mohammedsadikansari75@gmail.com>
2026-05-18 06:39:49 +0530

    Solved the problem of Contains Duplicate from Leetcode

--- Arrays/containsDuplicate.js
+++ Arrays/containsDuplicate.js
@@ -1,6 +1,15 @@
 // Problem: Contains Duplicate (#217) - https://leetcode.com/problems/contains-duplicate/description/
 
-var containsDuplicate = function(nums) {
-    let duplicate = new Set(nums)
-    return duplicate.size !== nums.length
+var containsDuplicate = function (nums) {
+    let set = new Set();
+
+    for (const element of nums) {
+        if(set.has(element)){
+            return true
+        }
+
+        set.add(element)
+    }
+
+    return false
 };
