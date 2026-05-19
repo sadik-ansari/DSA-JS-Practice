b1f99b826bbf7bdd68b140b9c4536f62a4c7f85b
sadik ansari <mohammedsadikansari75@gmail.com>
2026-05-19 07:08:48 +0530

    Solved the Maximmun Subarray problem

--- Arrays/maximumSubArray.js
+++ Arrays/maximumSubArray.js
@@ -1,20 +1,18 @@
 // Problem: Maximum SubArray (#53) - https://leetcode.com/problems/maximum-subarray/description/
 
 var maxSubArray = function (nums) {
-    if(nums.length <= 1) return nums[0]
-    let mid = Math.floor(nums.length / 2)
-    let sum = null
-    let i = 0
-    let j = mid - 1
-    let finalSum = -Infinity;
-    for(let i = 0; i < nums.length; i++){
+    if(nums.length === 1) return nums[0]
+    let sum = 0
+    let maxSum = -Infinity
+
+    for (let i = 0; i < nums.length; i++) {
         sum += nums[i]
-        if(finalSum < sum){
-            finalSum = sum
-        }
-        if(sum < 0){
+        maxSum = Math.max(sum, maxSum)
+
+        if (sum < 0) {
             sum = 0
         }
     }
-    return finalSum
+
+    return maxSum
 };
