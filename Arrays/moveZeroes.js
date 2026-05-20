18cee9d147a8a331a58bc25cfd2695e263d98c9a
sadik ansari <mohammedsadikansari75@gmail.com>
2026-05-20 06:11:36 +0530

    Solved a Leetcode problem of Move Zeroes

--- Arrays/moveZeroes.js
+++ Arrays/moveZeroes.js
@@ -1,15 +1,17 @@
 // Problem: Move Zeroes (#283) - https://leetcode.com/problems/move-zeroes/description/
 
-var moveZeroes = function(nums) {
-    let insertNonZero = 0
-    for(let i = 0; i < nums.length; i++){
-        if(nums[i] !== 0){
-            nums[insertNonZero] = nums[i];
-            insertNonZero++
+var moveZeroes = function (nums) {
+    let count = 0
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[count] = nums[i]
+            count++
         }
     }
-    while(insertNonZero < nums.length){
-        nums[insertNonZero] = 0;
-        insertNonZero++
+
+    while(count < nums.length) {
+        nums[count] = 0
+        count++
     }
 };
